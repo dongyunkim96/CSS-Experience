107475e4cd7508e89da92d3a1f96f66800c25324
dongyunkim96 <dongyunkim96@gmail.com>
2023-10-04 05:43:45 -0400

    days 12 done

--- my-app/src/pages/12/Page12.styled.js
+++ my-app/src/pages/12/Page12.styled.js
@@ -1,6 +1,32 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Frame = styled.div`
+const appear = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(15px); 
+  }
+`;
+
+const disappear = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateY(15px);
+  }
+  99% {
+    opacity: 0;
+    transform: translateY(0);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(500px);
+  }
+`;
+
+export const Container = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -11,97 +37,112 @@ export const Frame = styled.div`
   border-radius: 2px;
   box-shadow: 4px 8px 16px 0 rgba(0, 0, 0, 0.1);
   overflow: hidden;
-  background: #fff;
   color: #333;
-  font-family: "Open Sans", Helvetica, sans-serif;
+  font-family: 'Open Sans', Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: #4cb6de;
-  color: white;
-  z-index: 1;
+  background-color: #4cb6de;
 `;
 
 export const Center = styled.div`
-  width: min(85%, 400px);
-  margin: auto;
-  margin-top: 85px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
 `;
 
-export const Content = styled.p`
-  font-size: 1.5rem;
-  line-height: 1.5;
+export const MainText = styled.div`
+  color: white;
+  font-size: 24px;
+  line-height: 35px;
+  font-weight: 500;
+  font-family: Arial;
+  z-index: 10;
   position: relative;
+
+  &::before {
+    content: '„';
+    position: absolute;
+    color: #6ac2e3;
+    font-size: 250px;
+    top: -100px;
+    left: -24px;
+    z-index: -1;
+  }
 `;
 
-export const TooltipContainer = styled.div`
-  background: #287089;
-  padding: .3rem .4rem;
+export const ToolTipText = styled.span`
+  background-color: #286f8a;
+  padding: 5px 10px;
   cursor: pointer;
   position: relative;
 
-
   &::before {
-    bottom: -20px;
-    content: " ";
-    display: block;
-    height: 20px;
-    left: 0;
+    width: 50px;
+    height: 30px;
+    content: ' ';
     position: absolute;
-    width: 100%;
   }
 
-  &::after {
-    border-left: solid transparent 10px;
-    border-right: solid transparent 10px;
-    border-top: solid transparent 10px;
-    bottom: -9px;
-    content: " ";
-    height: 0;
-    left: 50%;
-    margin-left: -13px;
-    position: absolute;
-    width: 0;
+  &:hover .tooltip {
+    opacity: 1;
+    animation: ${appear} .6s forwards;
   }
 `;
 
-export const Tooltip = styled.span`
-  cursor: text;
-  font-size: 1rem;
-  width: 300px;
-  background: white;
-  bottom: 100%;
-  color: #fff;
-  display: block;
-  left: 50%;
-  margin-bottom: 15px;
-  opacity: 0;
-  pointer-events: none;
+export const Tooltip = styled.div`
   position: absolute;
-  transform: translate(-50%, -16px);
-  transition: all 0.3s ease-out;
-  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2s);
+  background-color: #286f8a;
+  height: 150px;
+  width: 250px;
+  top: -95px;
+  left: 10px;
+  cursor: unset;
+  opacity: 0;
+  transition: .6s;
+  transform: translateY(-500px);
+  animation: ${disappear} .6s forwards;
+`;
 
-  .tooltip__header {
-    color: #286f8a;
-    svg {
-        position: relative;
-        top: 3px;
-        margin-left: 0.5rem;
-        cursor: pointer;
-        transform-origin: center center;
-        transition: all .2s;
-        &:hover {
-            transform: scale(1.1);
-        }
-    }
+export const Header = styled.div`
+  background-color: white;
+  height: 34.5px;
+  color: #286f8a;
+  font-size: 16px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+
+  p {
+    margin-top: 21px;
+    margin-left: 10px;
   }
 
-  .tooltip__content {
-    background: #286f8a;
+  span {
+    margin-left: 5px;
+    transition: .2s;
+  }
+  span:hover {
+    transform: scale(1.1);
   }
 `;
 
-export const Author = styled.p`
-  font-style: italic;
-  font-size: 1.3rem;
+export const Content = styled.div`
+  font-size: 16px;
+  box-sizing: border-box;
+  padding: 12px 20px;
+  line-height: 24px;
+`;
+
+export const Triangle = styled.div`
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  border-top: 20px solid transparent;
+  left: 130px;
+  top: 122px;
 `;
